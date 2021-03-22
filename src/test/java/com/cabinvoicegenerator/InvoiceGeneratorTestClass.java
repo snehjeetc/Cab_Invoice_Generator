7705d16ba9c8e8f8c1fabc4354ac134c3e5b4052
snehjeetc12 <csnehjeet12@gmail.com>
2021-03-23 02:37:36 +0530

    Added test method to test minimum fare

--- src/test/java/com/cabinvoicegenerator/InvoiceGeneratorTestClass.java
+++ src/test/java/com/cabinvoicegenerator/InvoiceGeneratorTestClass.java
@@ -1,17 +1,30 @@
 package com.cabinvoicegenerator;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceGeneratorTestClass {
+    private InvoiceGenerator invoiceGenerator;
+
+    @Before
+    public void init(){
+        invoiceGenerator = new InvoiceGenerator();
+    }
 
     @Test
     public void givenDistanceAndTimeShould_ReturnTotalFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
+    }
 
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturn_MinimumFare(){
+        double distance = 0.0;
+        int time = 2;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
     }
 
 }
